f1139b62a9cd8ba782bb39c6de7c4be42c58c4ea
RainyN0077 <gotiyu0407@gmail.com>
2025-07-18 22:38:46 +0800

    尝试修复一个小bug

--- frontend/src/pages/ControlPanel.svelte
+++ frontend/src/pages/ControlPanel.svelte
@@ -13,63 +13,7 @@
 
     export let applyFont;
 
-// === 添加 IndexedDB 辅助函数 开始 ===
-const DB_NAME = 'FontStorage';
-const DB_VERSION = 1;
-const STORE_NAME = 'fonts';
 
-async function openDB() {
-    return new Promise((resolve, reject) => {
-        const request = indexedDB.open(DB_NAME, DB_VERSION);
-        
-        request.onerror = () => reject(request.error);
-        request.onsuccess = () => resolve(request.result);
-        
-        request.onupgradeneeded = (event) => {
-            const db = event.target.result;
-            if (!db.objectStoreNames.contains(STORE_NAME)) {
-                db.createObjectStore(STORE_NAME);
-            }
-        };
-    });
-}
-
-async function saveToIndexedDB(key, value) {
-    const db = await openDB();
-    const transaction = db.transaction([STORE_NAME], 'readwrite');
-    const store = transaction.objectStore(STORE_NAME);
-    store.put(value, key);
-    
-    return new Promise((resolve, reject) => {
-        transaction.oncomplete = () => resolve();
-        transaction.onerror = () => reject(transaction.error);
-    });
-}
-
-async function loadFromIndexedDB(key) {
-    const db = await openDB();
-    const transaction = db.transaction([STORE_NAME], 'readonly');
-    const store = transaction.objectStore(STORE_NAME);
-    const request = store.get(key);
-    
-    return new Promise((resolve, reject) => {
-        request.onsuccess = () => resolve(request.result);
-        request.onerror = () => reject(request.error);
-    });
-}
-
-async function deleteFromIndexedDB(key) {
-    const db = await openDB();
-    const transaction = db.transaction([STORE_NAME], 'readwrite');
-    const store = transaction.objectStore(STORE_NAME);
-    store.delete(key);
-    
-    return new Promise((resolve, reject) => {
-        transaction.oncomplete = () => resolve();
-        transaction.onerror = () => reject(transaction.error);
-    });
-}
-// === 添加 IndexedDB 辅助函数 结束 ===
 
     let activeTab = 'directives';
     let channelIdToClear = '';
